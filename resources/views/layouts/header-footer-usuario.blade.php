a88570500d1392818ed4004dce66beedf6a5a500
Carola <e_mo04@outlook.com>
2020-05-25 11:37:40 -0500

    n

--- resources/views/layouts/header-footer-usuario.blade.php
+++ resources/views/layouts/header-footer-usuario.blade.php
@@ -12,6 +12,7 @@
         <link rel="stylesheet" href="{{ asset('assets/fonts/ionicons.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome5-overrides.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/js/jquery.min.js') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
     </head>
     <body id="page-top" style="min-width: 450px;">
